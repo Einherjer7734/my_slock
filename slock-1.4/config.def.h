046eca29bd4b6e3487c4da9d5f3e3437db0187f0
Einherjer7734 <patrick.welzel1996@googlemail.com>
2022-02-06 13:22:03 +0100

    Patch mediakeys (not working with xmonad)

--- slock-1.4/config.def.h
+++ slock-1.4/config.def.h
@@ -1,6 +1,6 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nobody";
+static const char *user  = "patrick";
+static const char *group = "patrick";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
